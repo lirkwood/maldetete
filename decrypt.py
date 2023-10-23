9ad8244de26072c29dffb1fe1ab08ea31a115241
Linus Kirkwood <linuskirkwood@gmail.com>
2023-10-23 15:31:52 +1100

    Added demo day code

--- decrypt.py
+++ decrypt.py
@@ -3,6 +3,7 @@
 from paramiko.pkey import PKey
 
 
-def decrypt_pubkey(pubkey: PKey):
+def decrypt_pubkey(pubkey: PKey) -> bytes:
     if pubkey.algorithm_name == "RSA":
         print(pubkey.asbytes())
+    return pubkey.get_fingerprint()
