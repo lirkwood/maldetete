ff6bc1ac2290daaafa5a218daf02773e2f2ee502
Linus Kirkwood <linuskirkwood@gmail.com>
2023-10-30 19:02:01 +1100

    Limit decryption to only pubkeys <= 8 bits

--- server/decrypt.py
+++ server/decrypt.py
@@ -1,20 +1,28 @@
 #!/usr/bin/env python3
 
+from math import log2
 from re import VERBOSE
 from typing import Optional
 from cryptography.hazmat.primitives.asymmetric.rsa import RSAPrivateKey, RSAPublicKey
 from shors import shors
 
 
-def decrypt_pubkey(pubkey: RSAPublicKey) -> Optional[RSAPrivateKey]:
+def decrypt_pubkey(pubkey: RSAPublicKey):
     """
-    Derives a private RSA key from the public key.
+    Derives a private RSA key from the public key and prints its parameters.
 
     RSA keys have the satisfy the following equation:
     m^(e*d) ≡ m (mod n)
     Here m is the message, e and n are known are d is to be found.
     """
     pubints = pubkey.public_numbers()
+    if log2(pubints.n) > 8:
+        print(
+            "Factoring an integer larger than 8 bits is likely going to take a long time."
+            + " Try with something smaller."
+        )
+        return
+
     print(f"Decrypting public key with numbers {pubints}")
     factors = shors(pubints.n, attempts=20, neighborhood=0.01, numPeriods=2)
     if factors is None or factors is False:
